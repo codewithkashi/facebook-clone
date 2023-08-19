bab8aa964e6b0edb96d325da4529ab800acf15f4
CodeWithKashif <132544176+codewithkashi@users.noreply.github.com>
2023-08-19 17:49:36 +0500

    Update CreatePost.tsx

--- components/Post/CreatePost.tsx
+++ components/Post/CreatePost.tsx
@@ -13,7 +13,7 @@ const CreatePost = ({
   const { data: user } = useCurrentUser();
   const router = useRouter();
   return (
-    <div className="flex items-center my-4 mx-4 lg:mx-8 bg-white shadow-md rounded-md px-8 py-4 gap-2">
+    <div className="flex items-center my-4 mx-2 lg:mx-8 bg-white shadow-md rounded-md px-2 lg:px-8 py-4 gap-2">
       <Profile id={user?._id} imgUrl={user?.profileImage} />
       <div
         className=" flex items-center gap-2 w-full"
